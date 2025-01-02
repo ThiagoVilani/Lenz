126b2a9e08c2580f292bc911500e81bbeb55d355
ThiagoVilani <thiagovilani19@gmail.com>
2025-01-01 22:57:40 -0300

    Añado vaga validacion para el envio del formulario

--- solicitarPresupuesto.js
+++ solicitarPresupuesto.js
@@ -1,8 +1,11 @@
 document.getElementById("form-solicitar-presupuesto").addEventListener("click",()=>{
     event.preventDefault();
 })
-document.getElementById("enviar-form").addEventListener("click",()=>{
-    CartelPresupuesto();
+document.getElementById("enviar-form").addEventListener("click",async()=>{
+    const ok = await Validar();
+    if(ok){
+        CartelPresupuesto();
+    }
 })
 
 function CartelPresupuesto(){
@@ -11,4 +14,18 @@ function CartelPresupuesto(){
         text: 'Nos pondremos en contacto enseguida',
         icon: 'success'
       });
+}
+
+async function Validar(){
+    if(document.getElementById("input-nombre").value == "" ||
+        document.getElementById("input-telefono").value == "" ||
+        document.getElementById("input-ubicacion").value == ""){
+        Swal.fire({
+            title:"¡Campos vacios!",
+            text:"Debe completar todos los datos antes de solicitar el presupuesto",
+            icon:"error"
+        })
+        return false;
+    }
+    return true;
 }
