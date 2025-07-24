77d688dec7dd8c68717932caae98fab8679dab96
ThiagoVilani <thiagovilani19@gmail.com>
2025-07-24 09:09:25 -0300

    Agrego la url canonical e incorpora un pequeño cambio para limpiar las url

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ import { InsertarNavGrande } from "./herramientas.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   InsertarNavGrande(document,window);
+  
+  //  Esto era para probar hacer las URLs mas limpias
+
+  // console.log(document.getElementsByClassName("nav-link")[2]);
+  // document.getElementsByClassName("nav-link")[2].addEventListener("click",async()=>{
+  //   await fetch("http://127.0.0.1:3000/contacto");
+  //   console.log("click");
+  // });
 });
