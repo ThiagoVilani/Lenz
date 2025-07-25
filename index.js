fb7f86c777715681ddacc1ae77c791c7846a0cdb
ThiagoVilani <thiagovilani19@gmail.com>
2025-07-25 12:27:40 -0300

    Algunos cambios en la pagina de contacto

--- index.js
+++ index.js
@@ -2,12 +2,4 @@ import { InsertarNavGrande } from "./herramientas.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   InsertarNavGrande(document,window);
-  
-  //  Esto era para probar hacer las URLs mas limpias
-
-  // console.log(document.getElementsByClassName("nav-link")[2]);
-  // document.getElementsByClassName("nav-link")[2].addEventListener("click",async()=>{
-  //   await fetch("http://127.0.0.1:3000/contacto");
-  //   console.log("click");
-  // });
 });
