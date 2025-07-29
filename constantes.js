9873f86fd9301a8ec08af542f574fa5b5761da78
ThiagoVilani <thiagovilani19@gmail.com>
2025-07-29 08:56:10 -0300

    Cambio en las ruta de la nav para escritorio

--- constantes.js
+++ constantes.js
@@ -1,17 +1,20 @@
+const isDev = location.hostname === "localhost";
+export const API_URL = isDev ? "http://localhost:3000" : "https://lenzelectricidad.com.ar";
+
 export const nav = `<div class="container-fluid">
-        <a href="./index/" class="lenz-logo">
+        <a href="./index" class="lenz-logo">
           <img src="images/LenzLight.png" alt="lenz-logo">
         </a>
         <div id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./index/">Inicio</a>
+                <a class="nav-link active" aria-current="page" href="./index">Inicio</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./solicitarPresupuesto/">Solicitar presupuesto</a>
+                <a class="nav-link" href="./solicitarPresupuesto">Solicitar presupuesto</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./contacto/">Contacto</a>
+                <a class="nav-link" href="./contacto">Contacto</a>
               </li>
             </ul>
           </div>
